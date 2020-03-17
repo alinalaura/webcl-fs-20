86a30c6d2ece16d444013301cfee665d1fce6504
Alina <alina@dcas.ch>
2020-03-17 08:45:55 +0100

    add comments

--- week4/presentationModel/presentationModel.js
+++ week4/presentationModel/presentationModel.js
@@ -16,14 +16,14 @@ const Attribute = value => {
 
     const hasObs = name => observables.hasOwnProperty(name);
 
-    const getObs = (name, initValue = null) =>
+    const getObs = (name, initValue = null) => // lazy initialization
         hasObs(name)
             ? observables[name]
             : observables[name] = Observable(initValue);
 
     getObs(VALUE, value); // initialize the value at least
 
-    let   convert           = id ;
+    let   convert           = id ; // erspart zusätzlichen Check bei Aufruf von convert() weil die Funktion immer existiert
     const setConverter      = converter => {
         convert = converter;
         setConvertedValue(value);
@@ -31,7 +31,7 @@ const Attribute = value => {
     const setConvertedValue = val => getObs(VALUE).setValue(convert(val));
 
     // todo: this might set many validators without discharging old ones
-    const setValidator = validate => getObs(VALUE).onChange( val => getObs(VALID).setValue(validate(val)));
+    const setValidator = validate => getObs(VALUE).onChange( val => getObs(VALID).setValue(validate(val))); // wenn sich das Value ändert, wird es mit validate() überprüft und das Ergebnis in VALID eingetragen
 
     return { getObs, hasObs, setValidator, setConverter, setConvertedValue }
 };
